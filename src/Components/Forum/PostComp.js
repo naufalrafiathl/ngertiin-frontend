1235bb2f710dd95ac4a0d407948338fdffb8a1bb
shafiraputri01 <shafiraputri1001@gmail.com>
2021-12-01 23:46:29 +0700

    Fix forum

--- src/Components/Forum/PostComp.js
+++ src/Components/Forum/PostComp.js
@@ -5,6 +5,7 @@ import { useState,useEffect } from 'react';
 function PostComp(props) {
     
     const [post,setPost]=useState([]);
+    const [replies,setReplies]=useState([]);
     
     useEffect(() => {
         console.log("TES")
@@ -17,7 +18,20 @@ function PostComp(props) {
           }).catch(err => {
             // Do something for an error here
           });
-      }, [] );
+    }, [] );
+    
+    useEffect(() => {
+        console.log("TES")
+        fetch('Replies.json').then(response => {
+            return response.json();
+          }).then(data => {
+            // Work with JSON data here
+            setReplies(data)
+            console.log(data);
+          }).catch(err => {
+            // Do something for an error here
+          });
+    }, [] );
 
     return (
         <div className="container-forum">
@@ -39,8 +53,40 @@ function PostComp(props) {
                             <p><span class="iconify-inline" data-icon="ic:round-access-time"></span>  {post.waktu}</p>
                         </div>
                         <div>
-                            <p><span class="iconify-inline" data-icon="si-glyph:bubble-message-text"></span>  {post.child_post.length}</p>
+                            <p><span class="iconify-inline" data-icon="si-glyph:bubble-message-text"></span></p>
                         </div>
+                    </div>
+                    <div className="list-group">
+                        {replies ? replies.map((reply,index) => ( 
+                            <div className="post-card">
+                                <div className="post-header">
+                                    <h2><a className="post-link" href="/details-post">{reply.topik}</a></h2>
+                                    <a href="/update-post">
+                                        <button>
+                                        <span class="iconify-inline" data-icon="dashicons:edit"></span>
+                                            Update
+                                        </button>
+                                    </a>
+                                    <a href="/delete-post">
+                                        <button>
+                                        <span class="iconify-inline" data-icon="fluent:delete-24-filled"></span>
+                                            Hapus
+                                        </button>
+                                    </a>
+                                </div>
+                                <div className="post-body two-line-par ">
+                                    <p>{reply.isi}</p>                        
+                                </div>
+                                <div className="post-footer">
+                                    <div>
+                                        <p><span class="iconify-inline" data-icon="mdi:account"></span>  {reply.username}</p>
+                                    </div>
+                                    <div>
+                                        <p><span class="iconify-inline" data-icon="ic:round-access-time"></span>  {reply.waktu}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        )):"" }
                     </div>
                 </div>
                 <div className="forum-aside">
