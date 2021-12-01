d50dfcad82da12dde5dd3bcc174fb02cc00284d5
Naufal Rafi <naufalrafiathl@gmail.com>
2021-12-01 22:03:13 +0700

    remade profile, added favicon, and fix bugs

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import Dashboard from '../Components/DashboardProfile/Dashboard'
 import Navbar from '../Components/Navbar/Navbar'
 import ProfileCard from '../Components/ProfileCard/ProfileCard'
 function Profile() {
     return (
         <div>
             <Navbar isLogin={true} isRegistered={true}></Navbar>
-            <ProfileCard/>
+            <div className="header-profile">
+                <div className="header-left">
+                <ProfileCard/>
+                </div>
+                <div className="header-right">
+                <Dashboard></Dashboard>
+                </div>
+            </div>
+            
+            
         </div>
     )
 }
