de7a6dc029793ff34dc6ae5f69b8df39446ebf1e
muhammadhafizmm <m.hafizmaulana777@gmail.com>
2021-12-09 13:10:33 +0700

    master

---
+++ src/redux/modules/materi/thunks.js
@@ -0,0 +1,103 @@
+import ngertiApi from "../../../api/endpoints";
+import { refreshAccessToken } from "../auth/thunks";
+
+import {
+  setMapel, setModul, setMateriKuis,setMateri
+} from "../materi"
+
+export const related_mapel = ({jurusan}) => {
+    return (dispatch) => {
+      dispatch(
+        refreshAccessToken(() => {
+          ngertiApi.materi
+            .related_mapel(jurusan)
+            .then((res) => {
+              dispatch(setMapel(res.data))
+            })
+            .catch(() => console.log("error"))
+        })
+      )
+    }
+  }
+
+export const related_modul = ({mapel}) => {
+  return (dispatch) => {
+    dispatch(
+      refreshAccessToken(() => {
+        ngertiApi.materi
+          .related_modul(mapel)
+          .then((res) => {
+            dispatch(setModul(res.data))
+          })
+          .catch(() => console.log("error"))
+      })
+    )
+  }
+}
+
+export const related_soal = ({materi_kuis}) => {
+  return (dispatch) => {
+    dispatch(
+      refreshAccessToken(() => {
+        ngertiApi.materi
+          .related_soal(materi_kuis)
+          .then((res) => {
+            dispatch(setMateriKuis(res.data))
+          })
+          .catch(() => console.log("error"))
+      })
+    )
+  }
+}
+
+export const get_materi = ({materi_id}) => {
+  return (dispatch) => {
+    dispatch(
+      refreshAccessToken(() => {
+        ngertiApi.materi
+          .get_materi(materi_id)
+          .then((res) => {
+            console.log(res.data)
+            dispatch(setMateri(res.data))
+          })
+          .catch(() => console.log("error"))
+      })
+    )
+  }
+}
+
+export const get_try_out = ({try_out_id}) => {
+  return (dispatch) => {
+    dispatch(
+      refreshAccessToken(() => {
+        ngertiApi.try_out
+          .get_try_out(try_out_id)
+          .then((res) => {
+            console.log(res.data)
+            dispatch(setMateri(res.data))
+          })
+          .catch(() => console.log("error"))
+      })
+    )
+  }
+}
+
+export const create_try_out = ({
+  try_out_id,
+  materi,
+  jawaban
+}) => {
+  return (dispatch) => {
+    dispatch(
+      refreshAccessToken(() => {
+        ngertiApi.try_out
+          .get_try_out(materi_id)
+          .then((res) => {
+            console.log(res.data)
+            dispatch(setMateri(res.data))
+          })
+          .catch(() => console.log("error"))
+      })
+    )
+  }
+}
